e5b74bc180be01e6caced711f958f30841be311b
Molaei <molaie.web.developer@gmail.com>
2024-05-20 21:24:07 +0100

    create add product

--- src/components/addProductPage/addProduct.jsx
+++ src/components/addProductPage/addProduct.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import "./addProductStyle.css";
+import { Header } from "../homePage/header";
 
 export const AddProduct = () => {
     return ( 
         <>
-            <h1>addProduct Page</h1>
-            <Link to="/">Show All Product</Link>
+            <Header />
+            <div className="container">
+                <h2 className="title titr">ثبت محصول جدید</h2>
+                <span className="title">نام محصول:</span>
+                <input type="text" className="field" />
+                <span className="title">قیمت:</span>
+                <input type="text" className="field" />
+                <div className="chooseImage">
+                    
+                </div>
+            </div>
+            <Link to="/">Add</Link>
             ----
             <Link to="/">Cancel</Link>
         </>
