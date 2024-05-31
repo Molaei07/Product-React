6292dae9d253ecdf009c1409dd9f1cb0c4d25d69
Molaei <molaie.web.developer@gmail.com>
2024-05-31 18:36:31 +0100

    Add search on products

--- src/components/productItem/wrapperItem.jsx
+++ src/components/productItem/wrapperItem.jsx
@@ -1,24 +1,40 @@
 import Item from "./productItems";
 import "./items.css";
+import { useEffect, useState } from "react";
 
 const WrapperItem = ({ list, setList }) => {
+    const [searchedProducts, setSearchedProducts] = useState();
+
+    useEffect(() => {
+        setSearchedProducts(list)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [list])
+    
+    /* filtered data for search */
+    const searchInProducts = (event) => {
+        let searchValue = event.target.value
+        let filteredProducts = list.filter(el => el.name.indexOf(searchValue) !== -1);
+        setSearchedProducts(filteredProducts)
+    }
+
     return (
         <div className="content">
             <div className="header">
                 {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
                 <h1 className="title">All Products</h1>
-                <input className="searchBox" type="text" name="search" id="search" placeholder="Search..." />
+                <input onKeyUp={(event) => { searchInProducts(event) }} className="searchBox" type="text" name="search" id="search" placeholder="Search..." />
             </div>
-            {list.map((item,index) => (
-                <Item
-                    key={index}
-                    id={item.id}
-                    name={item.name}
-                    price={item.price}
-                    list={list}
-                    setList={setList}
-                />
-            ))}
+            {searchedProducts ?
+                searchedProducts.map((item, index) => (
+                    <Item
+                        key={index}
+                        id={item.id}
+                        name={item.name}
+                        price={item.price}
+                        list={list}
+                        setList={setList}
+                    />
+                )) : null}
         </div>
     );
 }
